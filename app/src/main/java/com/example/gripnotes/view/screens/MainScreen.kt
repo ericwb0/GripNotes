862dbfee2d4bdcd12dbdfe97c1ce21e31a86f0ef
ericwb0 <ericwb@email.sc.edu>
2025-04-30 17:04:54 -0400

    NotesViewModel

--- app/src/main/java/com/example/gripnotes/view/screens/MainScreen.kt
+++ app/src/main/java/com/example/gripnotes/view/screens/MainScreen.kt
@@ -3,7 +3,7 @@ package com.example.gripnotes.view.screens
 /**
  * Main screen composable.
  * It serves as a scaffold for the logged-in portion of the app.
- * Contains the bottom navigation bar and the main content area.
+ * Contains the top app bar and content area.
  *
  * @author ericwb0
  */
--- app/src/main/java/com/example/gripnotes/viewmodel/NotesViewModel.kt
+++ app/src/main/java/com/example/gripnotes/viewmodel/NotesViewModel.kt
@@ -1,7 +1,15 @@
 package com.example.gripnotes.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewModelScope
+import com.example.gripnotes.model.Note
+import com.example.gripnotes.model.RepositoryI
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 /**
  * ViewModel for the Notes screen.
@@ -9,6 +17,45 @@ import dagger.hilt.android.lifecycle.HiltViewModel
  * @author ericwb0
  */
 @HiltViewModel
-class NotesViewModel : ViewModel() {
+class NotesViewModel @Inject constructor(db: RepositoryI): ViewModel() {
+    private val _repo = db
 
+    /*
+     * UI state variables
+     */
+    private val _isLoading = MutableStateFlow(true)
+    val isLoading = _isLoading.asStateFlow()
+    private val _error = MutableStateFlow("")
+    val error = _error.asStateFlow()
+
+    /*
+     * Live feed of all notes for the user
+     */
+    val notes = _repo.notes
+        .also {
+            viewModelScope.launch {
+                it.collect { notes ->
+                    _isLoading.value = false
+                    if (notes.isEmpty()) {
+                        _error.value = "No notes found"
+                    } else {
+                        _error.value = ""
+                    }
+                }
+            }
+        }
+
+    fun addNote() {
+        val note = Note(title = "New Note") // Create just in case we need id later
+        viewModelScope.launch {
+            // All we do on this screen is add an empty note, data is handled in the editor
+            _repo.setNote(note)
+        }
+    }
+
+    fun deleteNote(id: String) {
+        viewModelScope.launch {
+            _repo.deleteNote(id)
+        }
+    }
 }
