34ad74a2e8c4c8ae686c70e407a1c40a12bcf440
ericwb0 <ericwb@email.sc.edu>
2025-05-03 17:54:56 -0400

    EditorScreen list

--- app/src/main/java/com/example/gripnotes/view/screens/EditorScreen.kt
+++ app/src/main/java/com/example/gripnotes/view/screens/EditorScreen.kt
@@ -1,10 +1,32 @@
 package com.example.gripnotes.view.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.gripnotes.model.NoteContentItem
 import com.example.gripnotes.viewmodel.EditorViewModel
 
 /**
@@ -23,5 +45,111 @@ fun EditorScreen(noteId: String) {
 
     LaunchedEffect(noteId) {
         editorViewModel.getNoteById(noteId)
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.Top
+    ) {
+        if (isLoading) {
+            CircularProgressIndicator()
+            Text(
+                text = "Loading note...",
+                modifier = Modifier.padding(16.dp),
+                style = MaterialTheme.typography.bodyLarge
+            )
+        } else if (error.isNotEmpty() || note == null) {
+            Text(
+                text = error.ifEmpty { "Note not found" },
+                modifier = Modifier.padding(16.dp),
+                style = MaterialTheme.typography.bodyLarge,
+                color = MaterialTheme.colorScheme.error
+            )
+        } else {
+            Text(
+                text = note!!.title,
+                modifier = Modifier.padding(16.dp),
+                style = MaterialTheme.typography.headlineMedium
+            )
+            HorizontalDivider(
+                modifier = Modifier.padding(8.dp),
+                thickness = 2.dp,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                horizontalAlignment = Alignment.Start
+            ) {
+                items(note!!.content.size) { index ->
+                    when (val contentItem = note!!.content[index]) {
+                        is NoteContentItem.TextItem -> {
+                            var text by remember { mutableStateOf(contentItem.text) }
+                            var isFocused by remember { mutableStateOf(false) }
+                            BasicTextField(
+                                value = text,
+                                onValueChange = { text = it },
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .padding(8.dp)
+                                    .onFocusChanged {
+                                        isFocused = it.isFocused
+                                        if (!it.isFocused) {
+                                            editorViewModel.updateContent(index,
+                                                NoteContentItem.TextItem(text))
+                                        }
+                                    },
+                                textStyle = MaterialTheme.typography.bodyLarge
+                            )
+                        }
+                        is NoteContentItem.CheckboxItem -> {
+                            Row(
+                                modifier = Modifier.fillMaxSize().padding(8.dp)
+                            ) {
+                                Checkbox(
+                                    checked = contentItem.isChecked,
+                                    onCheckedChange = { isChecked ->
+                                        editorViewModel.updateContent(
+                                            index,
+                                            NoteContentItem.CheckboxItem(contentItem.text, isChecked)
+                                        )
+                                    }
+                                )
+                                var text by remember { mutableStateOf(contentItem.text) }
+                                var isFocused by remember { mutableStateOf(false) }
+                                BasicTextField(
+                                    value = text,
+                                    onValueChange = { text = it },
+                                    modifier = Modifier
+                                        .fillMaxSize()
+                                        .padding(8.dp)
+                                        .onFocusChanged {
+                                            isFocused = it.isFocused
+                                            if (!it.isFocused) {
+                                                editorViewModel.updateContent(
+                                                    index,
+                                                    NoteContentItem.CheckboxItem(
+                                                        text, contentItem.isChecked)
+                                                )
+                                            }
+                                        },
+                                    textStyle = MaterialTheme.typography.bodyLarge
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+            Button(
+                modifier = Modifier.fillMaxWidth().padding(8.dp),
+                onClick = {
+                    editorViewModel.saveNote()
+                }
+            ) {
+                Text(text = "Save Note")
+            }
+        }
     }
 }
